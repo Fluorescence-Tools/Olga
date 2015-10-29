5f606e4c9f9a2b303c617e77fd028ef5846ad289
Mykola Dimura <mykola.dimura@gmail.com>
2015-10-29 18:10:27 +0100

    Fix a bug in total Force calculation.

--- av2restraints/main.cpp
+++ av2restraints/main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <iomanip>
 #include <thread>
+#include <set>
 
 #include <QCoreApplication>
 #include <QCommandLineParser>
@@ -51,7 +52,7 @@ struct Restrt {
 			f>>v[0]>>v[1]>>v[2];
 			coords.push_back(v);
 		}
-		std::cout<<"fsize:"<<fsize<<std::endl;
+
 		if(fsize-2.0*f.tellg()>0) {
 			for(int i=0; i<natoms; i++) {
 				Eigen::Vector3d v;
@@ -91,7 +92,7 @@ struct Restrt {
 
 		f<<footer;
 	}
-	int replaceCoords(const Eigen::Vector3d& oldp,const Eigen::Vector3d& newp)
+	/*void replaceCoords(const Eigen::Vector3d& oldp,const Eigen::Vector3d& newp)
 	{
 		const double epsilon=0.05;
 		double min=999.0;
@@ -105,22 +106,26 @@ struct Restrt {
 			}
 			if( dist<epsilon) {
 				v=newp;
-				return i;
+				return ;
 			}
 		}
 		std::cout<<"replaceCoords: atom not found! min_dsit="<<min
 			<<" idx="<<idx<<" oldXYZ="<<oldp.transpose()<<
 			  " newXYZ="<<newp.transpose()<<"\n";
-		return -1;
+		return ;
+	}*/
+	void setCoords(int atomId, const Eigen::Vector3d& c) {
+		coords[atomId-1]=c;
 	}
-	int atomIndex(const Eigen::Vector3d& pos) const
+
+	int atomId(const Eigen::Vector3d& pos) const
 	{
 		const double epsilon=0.05;
 		for(int i=0; i<coords.size(); ++i) {
 			const Eigen::Vector3d& v=coords[i];
 			double dist=(pos-v).norm();
 			if( dist<epsilon) {
-				return i;
+				return i+1;
 			}
 		}
 		return -1;
@@ -130,7 +135,8 @@ struct Restrt {
 		velocities.push_back(Eigen::Vector3d(0.0f,0.0f,0.0f));
 	}
 };
-
+struct NmrRestraint;
+std::ostream& operator<<(std::ostream& os, const NmrRestraint& r);
 struct NmrRestraint {
 	int _iat1, _iat2, _nstep1=1, _nstep2;
 	double r1, r2,r3,r4,rk2,rk3;
@@ -207,9 +213,9 @@ struct NmrRestraint {
 		}
 		for(const NmrRestraint& rst:vec) {
 			Eigen::Vector3d r=duIdPos.at(rst._iat1)-duIdPos.at(rst._iat2);
-			//std::cout<<rst._iat1<<"-"<<rst._iat2<<": "<<rst.force(r.norm())*69.4786<<" ";
-			totalForce[rst._iat1]+=rst.force(r.norm(),atstep1)*r.normalized();
-			totalForce[rst._iat2]-=totalForce[rst._iat1];
+			const auto& force1=rst.force(r.norm(),atstep1)*r.normalized();
+			totalForce[rst._iat1]+=force1;
+			totalForce[rst._iat2]-=force1;
 		}
 		return totalForce;
 	}
@@ -224,8 +230,9 @@ struct NmrRestraint {
 		const double inf=std::numeric_limits<double>::infinity();
 		double Fcap=vec.front().force(inf,atstep1);
 		Fcap=std::fabs(Fcap);
-		std::cout<<"Fcap1:"<<Fcap*69.4786<<std::endl;
+
 		auto totalF=totalForce(caped,duIdPos,atstep1);
+
 		auto pr = std::max_element(totalF.begin(), totalF.end(),
 					   [](const pair<int,Eigen::Vector3d>& p1,
 					   const pair<int,Eigen::Vector3d>& p2) {
@@ -245,10 +252,12 @@ struct NmrRestraint {
 				return p1.second.norm() < p2.second.norm(); });
 		}
 
+		std::cout<<"Total Force per DU (nstep1) =\n";print(totalF);std::cout<<std::endl;
+
 		atstep1=false;
 		Fcap=vec.front().force(inf,atstep1);
 		Fcap=std::fabs(Fcap);
-		std::cout<<"Fcap2:"<<Fcap*69.4786<<std::endl;
+
 		totalF=totalForce(caped,duIdPos,atstep1);
 		pr = std::max_element(totalF.begin(), totalF.end(),
 					   [](const pair<int,Eigen::Vector3d>& p1,
@@ -268,18 +277,29 @@ struct NmrRestraint {
 					      const pair<int,Eigen::Vector3d>& p2) {
 				return p1.second.norm() < p2.second.norm(); });
 		}
+		std::cout<<"Total Force per DU (nstep2) =\n";print(totalF);std::cout<<std::endl;
 
 		return caped;
 	}
-
 	static void print(const std::map<int,Eigen::Vector3d>& map)
 	{
 		for(const auto& pair:map)
 		{
-			std::cout<<pair.first<<": "<<pair.second.norm()*69.4786<<std::endl;
+			std::cout<<pair.first<<": |"<<pair.second.transpose()<<"|*69.4786 =\t"<<pair.second.norm()*69.4786<<std::endl;
+		}
+	}
+	static void print(const std::vector<NmrRestraint>& vec) {
+		for (const auto& r:vec) {
+			std::cout<<r<<std::endl;
 		}
 	}
 };
+std::ostream& operator<<(std::ostream& os, const NmrRestraint& r)
+{
+    os << r.toString();
+    return os;
+}
+
 const double errAnchor=2.0; //Angstrom
 const double FmaxMultAnchor=2.0; //2 fold
 int main(int argc, char *argv[])
@@ -321,7 +341,7 @@ int main(int argc, char *argv[])
 	using std::setprecision;
 	using std::setw;
 	pteros::System sys(pdbFileName.toStdString());
-	pteros::Selection sel=sys.select(std::string("resname DU"));
+	pteros::Selection selDU=sys.select(std::string("resname DU"));
 
 	Restrt restrt;
 	restrt.load(restartInFileName.toStdString());
@@ -354,9 +374,8 @@ int main(int argc, char *argv[])
 		if(pair.second->className()=="Positions") {
 			avs.push_back(pair.first);
 		}
-		//std::cout<<pair.first<<" "<<pair.second->name()<<std::endl;
 	}
-	const int numDUAtoms=sel.size();
+	const int numDUAtoms=selDU.size();
 	std::cout<<numDUAtoms<<" DU atoms, "<<avs.size()<<" positions and "<<distances.size()<<" distances"<<std::endl;
 
 	if(numDUAtoms!=avs.size() && numDUAtoms!=0) {
@@ -371,10 +390,10 @@ int main(int argc, char *argv[])
 		atom.resname="DU";
 		atom.mass=100.0f;
 		atom.charge=0.0f;
-		auto resids=sys.select("all").get_unique_resid();
+		auto resids=sys.select(std::string("all")).get_unique_resid();
 		auto it=std::max_element(resids.begin(),resids.end());
 		atom.resid=*it+1;
-		auto chains=sys.select("all").get_unique_chain ();
+		auto chains=sys.select(std::string("all")).get_unique_chain ();
 		atom.chain=*std::max_element(chains.begin(),chains.end())+1;
 		atom.chain=atom.chain=='!'?'B':atom.chain;
 		for(int i=0; i<avs.size(); i++) {
@@ -388,10 +407,10 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	sel.update();//apply()?
-	auto duXYZf=sel.get_xyz();
+	selDU.update();//apply()?
+	auto duXYZf=selDU.get_xyz();
 	auto duXYZd=duXYZf.cast<double>()*10.0;
-	std::cout<<"DUsD:\n"<<duXYZd<<"\n";
+	std::cout<<"DU coordinates read:\n"<<duXYZd<<"\n";
 
 	FrameDescriptor frame(pdbFileName.toStdString(),pdbFileName.toStdString());
 	storage.evaluate(frame,avs);
@@ -400,14 +419,14 @@ int main(int argc, char *argv[])
 	using std::map;
 	map<EvalId,pair<EvalId,EvalId>> dist2lps; //match distances to AVs
 	map<EvalId,std::string> lpNames;
-	map<EvalId,Distance> distOpt;
+	map<EvalId,Distance> distInfo;
 	for(const auto& dist:distances) {
 		EvalId lp1=storage.eval(dist).settingValue(1).value<EvalId>();
 		EvalId lp2=storage.eval(dist).settingValue(2).value<EvalId>();
 		lpNames[lp1]=storage.eval(lp1).name();
 		lpNames[lp2]=storage.eval(lp2).name();
 		dist2lps.insert({dist,std::make_pair(lp1,lp2)});
-		distOpt[dist]=static_cast<const EvaluatorDistance&>(storage.eval(dist)).distance();
+		distInfo[dist]=static_cast<const EvaluatorDistance&>(storage.eval(dist)).distance();
 		//std::cout<<dist<<" "<<lp1<<" "<<lp2<<std::endl;
 	}
 	while(!storage.ready()) {
@@ -415,38 +434,34 @@ int main(int argc, char *argv[])
 		std::cout<<"sleeping..."<<std::endl;
 	}
 
-	map<EvalId,int> duAtomIndex; //match AV_MPs to atomIDs
-	map<EvalId,Eigen::Vector3d> duAtomPos; //match  AV_MPs to atom position
+	map<EvalId,int> av2atomId; //match AV_MPs to atomIDs
+	//match  AV_MPs to atom position
+	std::map<int,Eigen::Vector3d> atomid2Pos;
 	for(const auto& avId:avs) {
 		TaskStorage::Result res=storage.getResult(frame,avId);
-		int curXYZind=duAtomIndex.size();
+		const int curXYZind=av2atomId.size();
 		Eigen::Vector3d oldcoords=duXYZd.col(curXYZind);
-		duAtomPos[avId]=oldcoords;
-		int idx=restrt.atomIndex(oldcoords);
-		duAtomIndex[avId]=idx;
-		std::cout<<idx<<" ";
-		if(!res) {continue;}
+		const int atomId=restrt.atomId(oldcoords);
+		av2atomId[avId]=atomId;
+		atomid2Pos[atomId]=oldcoords;
+
+		if(!res) {continue;}//somehow av calculation has failed, stay with old coords
 		std::shared_ptr<CalcResult<PositionSimulationResult>> calcpos;
 		calcpos=std::static_pointer_cast<CalcResult<PositionSimulationResult>>(res);
 		PositionSimulationResult pos=calcpos->get();
-		if(pos.empty()) {continue;}
+		if(pos.empty()) {continue;} //stay with old coords
 		Eigen::Vector3d newcoords=pos.meanPosition().cast<double>();
-		duAtomPos[avId]=newcoords;
-		restrt.replaceCoords(oldcoords,newcoords);
+		atomid2Pos[atomId]=newcoords;
+		//restrt.replaceCoords(oldcoords,newcoords);
+		restrt.setCoords(atomId,newcoords);
 		duXYZf.col(curXYZind)=newcoords.cast<float>()*0.1f;
 	}
 	std::cout<<std::endl;
+
 	restrt.save(restartOutFileName.toStdString());
 	if(parser.isSet("savepdb")) {
-		sel.set_xyz(duXYZf);
-		sys.select("all").write(parser.value("savepdb").toStdString());
-	}
-
-	map<int,Eigen::Vector3d> duIatPos; //match atomIDs to atom position
-	for(const auto& pair:duAtomIndex) {
-		const EvalId& eid=pair.first;
-		int atomIndex=pair.second;
-		duIatPos[atomIndex+1]=duAtomPos[eid];
+		selDU.set_xyz(duXYZf);
+		sys.select(std::string("all")).write(parser.value("savepdb").toStdString());
 	}
 
 	std::vector<NmrRestraint> nmrVec;
@@ -454,15 +469,16 @@ int main(int argc, char *argv[])
 	for(const auto& distId:distances) {
 		EvalId lp1=dist2lps.at(distId).first;
 		EvalId lp2=dist2lps.at(distId).second;
-		int iat1=duAtomIndex[lp1]+1;
-		int iat2=duAtomIndex[lp2]+1;
-		const Distance& opt=distOpt.at(distId);
+		int iat1=av2atomId[lp1];
+		int iat2=av2atomId[lp2];
+		const Distance& opt=distInfo.at(distId);
 
 		nmrVec.emplace_back(f1,f2,iat1,iat2,nstep2,opt);
 	}
 	if(!nocap) {
-		nmrVec=NmrRestraint::capForce(nmrVec,duIatPos);
+		nmrVec=NmrRestraint::capForce(nmrVec,atomid2Pos);
 	}
+
 
 	//add restraints from pseudoatoms to the molecule
 	for(const auto& avId:avs) {
@@ -476,10 +492,10 @@ int main(int argc, char *argv[])
 		vector<std::string> atNames=sel.get_name();
 		for(int c=0; c<anchXYZd.cols(); ++c) {
 			Eigen::Vector3d anchpos=anchXYZd.col(c);
-			int iat1=duAtomIndex.at(avId)+1;
-			int iat2=restrt.atomIndex(anchpos)+1;
+			int iat1=av2atomId.at(avId);
+			int iat2=restrt.atomId(anchpos);
 			Distance d;
-			d.setDistance((duAtomPos.at(avId)-anchpos).norm());
+			d.setDistance((atomid2Pos.at(iat1)-anchpos).norm());
 			d.setType("Rmp");
 			d.setErrNeg(errAnchor);
 			d.setErrPos(errAnchor);
