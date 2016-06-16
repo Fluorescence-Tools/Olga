e83c67a9c6031e5e4904f7e797355faa9da404ca
Mykola Dimura <mykola.dimura@gmail.com>
2016-06-16 10:46:41 +0200

    Fix used .svgz files to suppress runtime warnings

--- gui/main.cpp
+++ gui/main.cpp
@@ -6,8 +6,8 @@
 
 int main(int argc, char *argv[])
 {
-	std::cout<<"std::thread::hardware_concurrency()="<<
-		   std::thread::hardware_concurrency()<<std::endl;
+	/*std::cout<<"std::thread::hardware_concurrency()="<<
+		   std::thread::hardware_concurrency()<<std::endl;*/
 
 	QApplication a(argc, argv);
 	a.setApplicationVersion(APP_VERSION);
