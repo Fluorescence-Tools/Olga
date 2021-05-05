4f1ebd3dda6a6041cb9c733f45aade037c990943
Mykola Dimura <mykola.dimura@gmail.com>
2021-05-05 16:24:56 +0200

    Add .dll files to the packaging script
    Additional .dlls are needed for current distribution

--- src/pack_mingw.sh
+++ src/pack_mingw.sh
@@ -24,7 +24,7 @@ mkdir -p "${DESTDIR}"/imageformats/
 mkdir -p "${DESTDIR}"/iconengines/
 cp "${BUILDDIR}"/src/*/release/*.exe "${DESTDIR}"/
 cp "${BUILDDIR}"/src/irmsd/release/irmsd.dll "${DESTDIR}"/
-cp "${SYSLIBPREFIX}"/bin/lib{async++,bz2-1,freetype-6,gcc_s_seh-1,gcc_s_sjlj-1,glib-2.0-0,harfbuzz-0,iconv-2,intl-8,pcre2-16-0,pcre-1,pcre16-0,png16-16,pteros,stdc++-6,winpthread-1,boost_filesystem,graphite2,gomp-1,ssp-0}.dll "${DESTDIR}"/
+cp "${SYSLIBPREFIX}"/bin/lib{async++,bz2-1,freetype-6,gcc_s_seh-1,gcc_s_sjlj-1,glib-2.0-0,harfbuzz-0,iconv-2,intl-8,pcre2-16-0,fmt,pcre-1,pcre16-0,png16-16,pteros,tng_io,stdc++-6,winpthread-1,boost_filesystem,graphite2,gomp-1,ssp-0,spdlog}.dll "${DESTDIR}"/
 cp "${SYSLIBPREFIX}"/bin/{Qt5Core,Qt5Gui,Qt5Multimedia,Qt5Network,Qt5Svg,Qt5Widgets,zlib1}.dll "${DESTDIR}"/
 cp "${SYSLIBPREFIX}"/lib/qt/plugins/platforms/qwindows.dll "${DESTDIR}"/platforms/
 cp "${SYSLIBPREFIX}"/lib/qt/plugins/imageformats/{qsvg,qico,qtiff,qjpeg}.dll "${DESTDIR}"/imageformats/
